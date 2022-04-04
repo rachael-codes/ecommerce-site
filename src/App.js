e333cbd187f666987f0ea7e76898cd5b890cf0e7
Rachael <mcbride.rac@gmail.com>
2022-04-04 19:11:24 -0400

    Creates authentication page component and sign-in + sign-up forms

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
-import { Routes, Route } from "react-router-dom";
+import Authentication from "./routes/authentication/authentication.component";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -10,8 +12,9 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route index={true} element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
